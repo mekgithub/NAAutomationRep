c51bc38f5b525ccdf1f5461e11b2db71d4ace9dc
18327 <mkortak@gmail.com>
2019-09-17 19:52:41 -0500

    2. commit

--- src/NewGitTest.java
+++ src/NewGitTest.java
@@ -1,5 +1,8 @@
 public class NewGitTest {
     public static void main(String[] args) {
+        System.out.println("1. commit");
+        System.out.println("1.commit");
         System.out.println("2.Commit ");
+        System.out.println("3.Commit ");
     }
 }
