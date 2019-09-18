3a100eabb88091ed304dbcf88cffc3c034c8dba7
MEK <54605592+mekgithub@users.noreply.github.com>
2019-09-17 20:35:25 -0500

    Update NewGitTest.java

--- src/NewGitTest.java
+++ src/NewGitTest.java
@@ -5,5 +5,7 @@ public class NewGitTest {
         System.out.println("2.Commit ");
         System.out.println("3.Commit ");
         System.out.println("6.Commit ");
+         System.out.println("7.Commit ");
+    }
     }
 }
